a6661dac796111d68e2095e2098ad65ca8636c63
cloutsider <cloutsider@4fe36df8-9bf8-894d-b5ad-10f2700a689d>
2011-08-30 10:09:35 +0000

    git-svn-id: http://localhost:7903/svn/res/trunk@56 4fe36df8-9bf8-894d-b5ad-10f2700a689d

--- testLua/a.lua
+++ testLua/a.lua
@@ -13,6 +13,46 @@ lc:f1v(222);
 print("lua  " .. lc:f2(1, 2))
 lc:f2v(223, 224);
 
+testabc(33, "abc")
+
+local za1, za2 = lc:luaMemberFunction(22, 11)
+print("lua  za1 = " .. za1 .. "  za2 = " .. za2)
+
+function lua_test()
+	print("lua  lua_test");
+end
+
+function lua_return_int()
+	return 66;
+end
+
+function lua_param1(i)
+	print("lua  lua_param1 i = " .. i)
+end
+
+function lua_param2(i1, i2)
+	print("lua  lua_param2 i1 = " .. i1 .. " i2 = " .. i2)
+end
+
+
+function lua_param3(i1, i2, i3)
+	print("lua  lua_param3 i1 = " .. i1 .. " i2 = " .. i2  .. " i3 = " .. i3)
+end
+
+
+function lua_param4(i1, i2, i3, i4)
+	print("lua  lua_param4 i1 = " .. i1 .. " i2 = " .. i2  .. " i3 = " .. i3 .. " i4 = " .. i4)
+end
+
+function lua_param5(i1, i2, i3, i4, i5)
+	print("lua  lua_param5 i1 = " .. i1 .. " i2 = " .. i2  .. " i3 = " .. i3 .. " i4 = " .. i4  .. " i5 = " .. i5)
+end
+
+
+function lua_param6(i1, i2, i3, i4, i5, i6)
+	print("lua  lua_param6 i1 = " .. i1 .. " i2 = " .. i2  .. " i3 = " .. i3 .. " i4 = " .. i4 .. " i5 = " .. i5 .. " i6 = " .. i6)
+end
+
 
 --[[
 print(AAA_1)
